fb1bd6f79bb05eb8136f7ac0e7cd80583b499c36
Zoran Zaric <zz@zoranzaric.de>
2010-09-08 01:32:15 +0200

    Adds bup import rsnapshot.
    A simple shellscript that iterates over the directory
    structure of a rsnapshot backup repository and adds each
    snapshot for every source.
    
    bup import rsnapshot makes use of the new --date and --strip options.

---
+++ cmd/import-rsnapshot-cmd.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# bup-import-rsnapshot.sh
+
+# Does an import of a rsnapshot archive.
+
+usage() {
+	echo "Usage: bup import-rsnapshot <path to rsnapshot's snapshot_root>"
+	exit -1
+}
+
+[ "$#" -eq 1 ] || usage
+
+if [ ! -e "$1/." ]; then
+	echo "$1 isn't a directory!"
+	exit -1
+fi
+
+ABSPATH=`readlink -f "$1"`
+
+for SNAPSHOT in "$ABSPATH/"*; do
+	if [ -e "$SNAPSHOT/." ]; then
+		for BRANCH_PATH in "$SNAPSHOT/"*; do
+			if [ -e "$BRANCH_PATH/." ]; then
+				# Get the snapshot's ctime
+				DATE=`stat -c %Z "$BRANCH_PATH"`
+				BRANCH=`basename "$BRANCH_PATH"`
+                TMPIDX="/tmp/$BRANCH"
+
+				bup index -ux \
+					$BRANCH_PATH/
+				bup save \
+					--strip=$BRANCH_PATH \
+					--date=$DATE \
+					-n $BRANCH \
+					$BRANCH_PATH/
+
+				if [ -e "$TMPIDX" ]; then
+					rm "$TMPIDX"
+				fi
+			fi
+		done
+	fi
+done

b504b7e6798556b09533ed747f2481ab835ef1e4
Zoran Zaric <zz@zoranzaric.de>
2011-08-22 04:45:15 +0200

    Add metadata saving

--- cmd/inotify-cmd.py
+++ cmd/inotify-cmd.py
@@ -3,7 +3,7 @@ import pyinotify
 import os
 import sys
 
-from bup import git, options, index, save
+from bup import git, options, index, save, metadata
 
 BUP_DIR = os.environ.get('BUP_DIR')
 
@@ -28,23 +28,36 @@ wm = pyinotify.WatchManager()
 mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY
 
 def backup(path):
+    metadata_path = os.path.join(path, ".bup.meta")
+    metadata_file_handler = open(metadata_path, 'w')
+    metadata.save_tree(output_file=metadata_file_handler,
+                       paths=[path],
+                       recurse=True,
+                       write_paths=True,
+                       save_symlinks=True,
+                       xdev=False)
     index.index(paths=[path])
     save.save(quiet=True, name = name, paths=[path])
+    os.unlink(metadata_path)
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
-        print "Creating:", event.pathname
-        # TODO new directories should be added to the watchlist
-        backup(event.pathname)
+        if not event.pathname.endswith(".bup.meta"):
+            print "Creating:", event.pathname
+            # TODO new directories should be added to the watchlist
+            backup(event.pathname)
 
     def process_IN_DELETE(self, event):
-        print "Removing:", event.pathname
-        # if a file or directory is deleted we have to index and save its parent
-        backup(os.path.join(event.pathname, os.path.pardir))
+        if not event.pathname.endswith(".bup.meta"):
+            print "Removing:", event.pathname
+            # if a file or directory is deleted we have to index and save its
+            # parent
+            backup(os.path.join(event.pathname, os.path.pardir))
 
     def process_IN_MODIFY(self, event):
-        print "Modifying:", event.pathname
-        backup(event.pathname)
+        if not event.pathname.endswith(".bup.meta"):
+            print "Modifying:", event.pathname
+            backup(event.pathname)
 
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler, timeout=10)

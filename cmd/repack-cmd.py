811d5c68a585f88ceeda77d549b648e0c39eafa0
Zoran Zaric <zz@zoranzaric.de>
2011-09-23 02:28:19 +0200

    Add repack command

---
+++ cmd/repack-cmd.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import sys, os
+from bup import git, options
+from bup.helpers import *
+
+class NeededObjects():
+    def __init__(self, pack_idx_list):
+        self.pack_idx_list = pack_idx_list
+        self.pack_bitarrays = dict()
+        for pack in self.pack_idx_list.packs:
+            self.pack_bitarrays[pack.name] = BitArray()
+
+    def __contains__(self, sha):
+        for pack in self.pack_idx_list.packs:
+            idx = pack._idx_from_hash(sha.decode('hex'))
+            if idx in self.pack_bitarrays[pack.name]:
+                return True
+        return False
+
+    def add(self, sha):
+        for pack in self.pack_idx_list.packs:
+            idx = pack._idx_from_hash(sha.decode('hex'))
+            self.pack_bitarrays[pack.name].add(idx)
+
+    def get_bitarray_for_pack(self, name):
+        if name in self.pack_bitarrays:
+            return self.pack_bitarrays[name]
+        else:
+            return None
+
+def traverse_commit(cp, sha_hex, needed_objects):
+    if sha_hex not in needed_objects:
+        needed_objects.add(sha_hex)
+        yield ('commit', sha_hex)
+
+        it = iter(cp.get(sha_hex))
+        type = it.next()
+        assert(type == 'commit')
+        tree_sha = "".join(it).split("\n")[0].lstrip("tree ").rstrip(" ")
+        for obj in traverse_objects(cp, tree_sha, needed_objects):
+            yield obj
+
+
+def traverse_objects(cp, sha_hex, needed_objects):
+    if sha_hex not in needed_objects:
+        needed_objects.add(sha_hex)
+        it = iter(cp.get(sha_hex))
+        type = it.next()
+
+        if type == 'commit':
+            yield ('commit', sha_hex)
+
+            tree_sha = "".join(it).split("\n")[0].lstrip("tree ").rstrip(" ")
+
+            for obj in traverse_objects(cp, tree_sha, needed_objects):
+                yield obj
+
+        if type == 'tree':
+            for (mode,mangled_name,sha) in git.tree_decode("".join(it)):
+                yield ('tree', sha_hex)
+
+                for obj in traverse_objects(cp, sha.encode('hex'),
+                                            needed_objects):
+                    yield obj
+
+        elif type == 'blob':
+            yield ('blob', sha_hex)
+
+
+optspec = """
+bup repack
+--
+q,quiet    don't show progress meter
+#,compress=  set compression level to # (0-9, 9 is highest) [1]
+"""
+o = options.Options(optspec)
+(opt, flags, extra) = o.parse(sys.argv[1:])
+
+git.check_repo_or_die()
+
+handle_ctrl_c()
+
+cp = git.CatPipe()
+
+opt.progress = (istty2 and not opt.quiet)
+refs = git.list_refs()
+refnames = [name for name, sha in refs]
+
+pl = git.PackIdxList(git.repo('objects/pack'))
+
+needed_objects = NeededObjects(pl)
+
+# Find needed objects reachable from commits
+for refname in refnames:
+    if not refname.startswith('refs/heads/'):
+        continue
+    log('Traversing %s to find needed objects...\n' % refname[11:])
+    traversed_objects_counter = 0
+    for date, sha in ((date, sha.encode('hex')) for date, sha in
+                      git.rev_list(refname)):
+        for type, sha_ in traverse_commit(cp, sha, needed_objects):
+            traversed_objects_counter += 1
+            qprogress('Traversing objects: %d\r' % traversed_objects_counter)
+
+# Find needed objects reachable from tags
+tags = git.tags()
+if len(tags) > 0:
+    for key in tags:
+        log('Traversing tag %s to find needed objects...\n' % ", ".join(tags[key]))
+        for type, sha in traverse_commit(cp, sha, needed_objects):
+            traversed_objects_counter += 1
+            qprogress('Traversing objects: %d\r' % traversed_objects_counter)
+progress('Traversing objects: %d, done.\n' % traversed_objects_counter)
+
+blob_writer = git.PackWriter(compression_level=opt.compress)
+w = git.PackWriter(compression_level=opt.compress)
+
+log('Writing new packfiles...\n')
+written_shas = set()
+written_object_counter = 0
+for pack in pl.packs:
+    ba = needed_objects.get_bitarray_for_pack(pack.name)
+    for offset, sha in pack.hashes_sorted_by_ofs():
+        idx = pack._idx_from_hash(sha)
+        if idx in ba:
+            if not sha in written_shas:
+                it = iter(cp.get(sha.encode('hex')))
+                type = it.next()
+                content = "".join(it)
+                if type == 'blob':
+                    blob_writer._write(sha, type, content)
+                else:
+                    w._write(sha, type, content)
+                written_shas.add(sha)
+                written_object_counter += 1
+                qprogress('Writing objects: %d\r' % written_object_counter)
+    os.unlink(pack.name)
+    os.unlink(pack.name[:-3] + "pack")
+progress('Writing objects: %d, done.\n' % written_object_counter)
+
+blob_writer.close()
+w.close()
+

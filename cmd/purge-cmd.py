4a7883db72263686dec1dfc34c19cda375c63789
Zoran Zaric <zz@zoranzaric.de>
2011-09-24 15:11:07 +0200

    Improve output

--- cmd/purge-cmd.py
+++ cmd/purge-cmd.py
@@ -41,6 +41,8 @@ f,force    don't ask, just do it
 n,name=    name of backup set to purge from (if any specific)
 d,dry-run  don't do anything, just print what would be done
 c,commit   purge a specific commit
+v,verbose  increase log output (can be used more than once)
+q,quiet    don't show progress meter
 #,compress=  set compression level to # (0-9, 9 is highest) [1]
 """
 o = options.Options(optspec)
@@ -52,6 +54,7 @@ handle_ctrl_c()
 
 cp = git.CatPipe()
 
+opt.progress = (istty2 and not opt.quiet)
 if opt.name:
     refnames = ['refs/heads/%s' % opt.name]
 else:
@@ -69,6 +72,7 @@ for pack in pl.packs:
 for refname in refnames:
     if not refname.startswith('refs/heads/'):
         continue
+    log('Traversing %s to find needed objects...\n' % refname[11:])
     for date, sha in cp.get_commits(refname):
         # TODO Add date based filtering
         for type, sha_ in cp.traverse_commit(sha):
@@ -80,6 +84,7 @@ for refname in refnames:
 tags = git.tags()
 if len(tags) > 0:
     for key in tags:
+        log('Traversing tag %s to find needed objects...\n' % ", ".join(tags[key]))
         for type, sha in cp.traverse_commit(sha):
             for pack in pl.packs:
                 idx = pack._idx_from_hash(sha_.decode('hex'))
@@ -88,8 +93,9 @@ if len(tags) > 0:
 blob_writer = git.PackWriter(compression_level=opt.compress)
 w = git.PackWriter(compression_level=opt.compress)
 
+log('Writing new packfiles...\n')
 written_shas = set()
-duplicates = []
+written_object_counter = 0
 for pack in pl.packs:
     ba = pack_bitarrays[pack.name]
     for offset, sha in pack.hashes_sorted_by_ofs():
@@ -103,16 +109,12 @@ for pack in pl.packs:
                     blob_writer._write(sha, type, content)
                 else:
                     w._write(sha, type, content)
-                log('.')
                 written_shas.add(sha)
-            else:
-                duplicates.append(sha.encode('hex'))
+                written_object_counter += 1
+                qprogress('Writing objects: %d\r' % written_object_counter)
     os.unlink(pack.name)
     os.unlink(pack.name[:-3] + "pack")
+progress('Writing objects: %d, done.\n' % written_object_counter)
 
 w.close()
-
-log("\n")
-print "Number of written objects: ", len(written_shas)
-print "Duplicates: ", duplicates
 

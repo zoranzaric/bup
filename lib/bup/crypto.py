36ce1e990eef4d9cd40fe0a8216250b79d2a539e
Julian Wälde <jwaelde@cdc.informatik.tu-darmstadt.de>
2012-03-28 17:08:36 +0200

    introducing the first shot at crypto
    key derivation is custom right now ... review needed!
    TODO: salt is still static
    TODO: iteration count is still static
    TODO: tests!

---
+++ lib/bup/crypto.py
@@ -0,0 +1,69 @@
+
+import nacl
+import hmac
+import hashlib
+import getpass
+import sys
+
+hashkey = None
+enckey = None
+
+
+def encrypt_buffer(content,key):
+    iv  = nacl.randombytes(24)
+    content = iv+nacl.crypto_secretbox(str(content),iv,key)
+    return buffer(content)
+
+def decrypt_buffer(content,key):
+    content = str(content)
+    iv = content[:24]
+    content = content[24:]
+    content = nacl.crypto_secretbox_open(content,iv,key)
+    return content
+
+def sha512(msg):
+    return hashlib.sha512(msg).digest()
+
+def sha256(msg):
+    return hashlib.sha256(msg).digest()
+
+def kdf(passphrase,salt):
+    """
+     given a passphrase and a salt this function derivates 
+     two keys that will later be used for encryption/hashblinding
+
+     this is up for review ... alternatives are pbkdf2 and b/scrypt
+
+     TODO: make number of iterations variable (benchmark it on bup init)
+    """
+    U = ""
+    for i in xrange(65536):
+        U = sha256(U + salt + passphrase + str(i))
+    return [sha256("0"+U),sha256("1"+U)]
+
+
+def askpassphrase():
+    global enckey
+    global hashkey
+    pw1 = "pw1"
+    pw2 = "pw2"
+    while pw1 != pw2:
+        pw1 = getpass.getpass("Enter Passphrase:")
+        pw2 = getpass.getpass("Repeat Passphrase:")
+        if (pw1 != pw2):
+            sys.stderr.write("Passphrases did not match!\n")
+    keys = kdf(pw1,"bupsalt") # TODO: get salt from salt file in repository
+    hashkey = keys[0]
+    enckey = keys[0]
+
+    # two lazy getter 
+
+def getencryptionkey():
+    if not enckey:
+        askpassphrase()
+    return enckey
+
+def gethashkey():
+    if not hashkey:
+        askpassphrase()
+    return hashkey
